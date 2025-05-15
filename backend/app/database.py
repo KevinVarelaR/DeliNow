d4a9e11c510ea4dbda0dfa711cd30600e82fb7f2
Kevin Vinicio Varela Rojas <kevin03varela@gmail.com>
2025-05-14 22:00:50 -0600

    feat: old db deleted new db added

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-databaseURL = "postgresql://postgres:1234@localhost:5432/project2"
+databaseURL = "postgresql://postgres:OzDbKfdbwBTNYhaqvvzhvryfZZsYpSEf@yamanote.proxy.rlwy.net:27184/railway"
 
 engine = create_engine(databaseURL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
